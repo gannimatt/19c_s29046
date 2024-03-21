2f7b256e1cfb3a91f8c594fc19b4a278c136a2cc
ganimatt <123307037+ganimatt@users.noreply.github.com>
2024-03-21 20:32:47 +0100

    Task 4

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-# Using list comprehension to generate a list of squares of numbers from 1 to 10
-squares = [x**2 for x in range(1, 11)]
-print(squares)
-
-
-def generate_squares(start, end):
-    squares = [x**2 for x in range(start, end + 1)]
-    return squares
-
-squares_1_to_10 = generate_squares(1, 10)
-print(squares_1_to_10)
-
+import math
 class SquareGenerator:
     def generate_squares(self, start, end):
-        squares = [x**2 for x in range(start, end + 1)]
+
+        squares = [x ** 2 for x in range(start, end + 1)]
         return squares
+
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
 
 # Example usage:
 square_gen = SquareGenerator()
 squares_1_to_10 = square_gen.generate_squares(1, 10)
-print(squares_1_to_10)
+print("Generated squares:", squares_1_to_10)
+
+square_roots = square_gen.calculate_square_roots(squares_1_to_10)
+print("Square roots:", square_roots)
