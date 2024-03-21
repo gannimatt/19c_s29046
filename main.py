004cc8a0eeb00cfa706de42540b240c9ff2dc313
ganimatt <123307037+ganimatt@users.noreply.github.com>
2024-03-21 20:42:42 +0100

    Task 8

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from square_generator.square_generator import SquareGenerator, RangeError
 
+
+class CubicGenerator(SquareGenerator):
+    def generate_squares(self, start, end):
+
+        if end < start:
+            raise RangeError("End of range cannot be less than start.")
+
+        cubes = [x ** 3 for x in range(start, end + 1)]
+        return cubes
+
+
+# Example usage:
+cubic_gen = CubicGenerator()
+cubes_1_to_5 = cubic_gen.generate_squares(1, 5)
+print("Generated cubes:", cubes_1_to_5)
+
 # Example usage:
 square_gen = SquareGenerator()
 try:
