c825ea73038d87cce7c5d79007fe756920182c8c
ganimatt <123307037+ganimatt@users.noreply.github.com>
2024-03-21 20:27:49 +0100

    Task 2

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Using list comprehension to generate a list of squares of numbers from 1 to 10
 squares = [x**2 for x in range(1, 11)]
-
-# Print the list of squares
 print(squares)
+
+
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
+
+squares_1_to_10 = generate_squares(1, 10)
+print(squares_1_to_10)
+
