b34411df727098f843035039f5b0a02f4389bf46
ganimatt <123307037+ganimatt@users.noreply.github.com>
2024-03-21 20:26:45 +0100

    Task 1

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+# Using list comprehension to generate a list of squares of numbers from 1 to 10
+squares = [x**2 for x in range(1, 11)]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Print the list of squares
+print(squares)
